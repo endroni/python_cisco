b0f86cdc76b1d7f38e4faf3afb663f3a8dbc1f53
endroni <endroni@yahoo.com.br>
2023-02-06 14:28:55 -0300

    modulo 2, lab 2.1.1.6

---
+++ Modulo2/lab_2_1_1_6.py
@@ -0,0 +1,7 @@
+﻿print("Hello, Python!")
+print("Rubem")
+#print(Rubem)
+#print"Rubem"
+#print("Rubem") print("SENAI") print("Belo Horizonte/MG")
+print("Rubem"), print("SENAI"), print("Belo Horizonte/MG")
+print('Rubem')
